1b2b9869426a69b9c9e944f8be0d2df8a4ced5d3
William Ochieng Odhiambo <williamochieng615@gmail.com>
2023-07-13 13:14:03 -0700

    sizeof program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 printf("Size of char: %zu byte(s)\n", sizeof(char));
+	 printf("Size of int: %zu byte(s)\n", sizeof(int));
+	 printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	 printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	 printf("Size of float: %zu byte(s)\n", sizeof(float));
+	 return (0);
+}
+

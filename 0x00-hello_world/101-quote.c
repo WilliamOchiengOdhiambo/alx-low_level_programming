d9fa6715769b16a580b9c60654827675ad5339e1
William Ochieng Odhiambo <williamochieng615@gmail.com>
2023-07-14 02:40:47 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - C program that prints a statement in a new line to the standard output
+ *
+ * Return:1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stdout);
+	return (1);
+}
+
